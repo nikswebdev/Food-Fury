beddd8c9c94c20a07dabc66fc93905e0852802cd
nikswebdev <nikswebdevacc@gmail.com>
2022-01-06 06:11:37 -0600

    updated styles on user feedback

--- src/Reviews/Feedback.js
+++ src/Reviews/Feedback.js
@@ -1,12 +1,12 @@
 import classes from './Feedback.module.css';
 import LogoIcon from '../global/assets/LogoIcon';
 
-const Feedback = () => {
+const Feedback = (props) => {
   return (
     <div className={classes.feedback}>
-      <div className={classes.feedback__name}>Kiani J</div>
+      <div className={classes.feedback__name}>{props.name}</div>
       <div className={classes.feedback__details}>
-        <div className={classes.feedback__stars}>&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+        <div className={classes.feedback__stars}> &#9733; &#9733; &#9733; &#9733;</div>
         <span>&bull;</span>
         <div className={classes.feedback__date}>12/1/21</div>
         <div className={classes.feedback__device}>Reviewed on  &nbsp;<LogoIcon /></div>
--- src/Reviews/ReviewBox.js
+++ src/Reviews/ReviewBox.js
@@ -2,15 +2,24 @@ import classes from './ReviewBox.module.css';
 import HeaderMedium from '../UI/HeaderMedium';
 import Stars from './Stars/Stars';
 import Feedback from './Feedback';
+import ScrollContainer from '../UI/ScrollContainer';
 
-const ReviewBox = ()=>{
-    return (
-        <div className={classes['review-box']}>
-          <HeaderMedium>What people are saying</HeaderMedium>
-          <Stars/>
-          <Feedback/>
-        </div>
-      );
-}
+const ReviewBox = () => {
+  return (
+    <div className={classes['review-box']}>
+      <HeaderMedium>What people are saying</HeaderMedium>
+      <Stars />
+      <ScrollContainer noPadding = {true}>
+        <Feedback stars = {5}/>
+        <Feedback stars = {2}/>
+        <Feedback stars = {4}/>
+        <Feedback stars = {3}/>
+        <Feedback stars = {4}/>
+        <Feedback stars = {4}/>
+        <Feedback stars = {5}/>
+      </ScrollContainer>
+    </div>
+  );
+};
 
 export default ReviewBox;
