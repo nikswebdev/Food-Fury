83dfeb4d13c8148f869e5ba714b496acbbc400fa
nikswebdev <nikswebdevacc@gmail.com>
2022-01-06 17:40:00 -0600

    finished styling reviews section

--- src/Reviews/Feedback.js
+++ src/Reviews/Feedback.js
@@ -2,16 +2,29 @@ import classes from './Feedback.module.css';
 import LogoIcon from '../global/assets/LogoIcon';
 
 const Feedback = (props) => {
+
+  const stars = [];
+  const setStars = ()=>{
+    for(let i = 0; i < props.stars; i++){
+      stars.push(1);
+    }
+  }
+
+  setStars();
+  
+
   return (
     <div className={classes.feedback}>
       <div className={classes.feedback__name}>{props.name}</div>
       <div className={classes.feedback__details}>
-        <div className={classes.feedback__stars}> &#9733; &#9733; &#9733; &#9733;</div>
+        <div className={classes.feedback__stars}> {stars.map(star => {
+          return <div className={classes.feedback__star}>&#9733;</div>
+          })}</div>
         <span>&bull;</span>
-        <div className={classes.feedback__date}>12/1/21</div>
+        <div className={classes.feedback__date}>{props.date}</div>
         <div className={classes.feedback__device}>Reviewed on  &nbsp;<LogoIcon /></div>
       </div>
-      <div className={classes.feedback__comment}>Great service and food!</div>
+      <div className={classes.feedback__comment}>{props.comment}</div>
     </div>
   );
 };
--- src/Reviews/ReviewBox.js
+++ src/Reviews/ReviewBox.js
@@ -4,19 +4,74 @@ import Stars from './Stars/Stars';
 import Feedback from './Feedback';
 import ScrollContainer from '../UI/ScrollContainer';
 
+const DUMMY_USERS = [{
+  id: Math.random(),
+  name: 'Jose',
+  date: '12/21/21',
+  comment: 'Great service and food',
+  stars:5,
+},
+{
+  id: Math.random(),
+  name: 'Rick p',
+  date: '12/28/21',
+  comment: 'Burgers are awesome',
+  stars: 4,
+},
+{
+  id: Math.random(),
+  name: 'Anne',
+  date: '1/14/22',
+  comment: 'Food was OK',
+  stars: 3,
+},
+{
+  id: Math.random(),
+  name: 'Damien Dozier',
+  date: '1/15/22',
+  comment: 'Yeeeeah!!!!!!',
+  stars: 5,
+},
+{
+  id: Math.random(),
+  name: 'Jessica P',
+  date: '1/25/22',
+  comment: 'Server was hilarious! 5/5!',
+  stars: 5,
+},
+{
+  id: Math.random(),
+  name: 'Karen',
+  date: '1/31/22',
+  comment: 'If I could vote a zero, I WOULD!!',
+  stars: 1,
+},
+{
+  id: Math.random(),
+  name: 'Greg',
+  date: '2/5/22',
+  comment: 'My wife was happy so I guess I am:)',
+  stars: 4,
+},
+]
+
 const ReviewBox = () => {
+
+  const displayFeedBack = ()=>{
+    return(
+      DUMMY_USERS.map(user =>{
+        return <Feedback stars = {user.stars} date = {user.date}
+        name = {user.name} comment = {user.comment}/>
+      })
+    );
+  }
+
   return (
     <div className={classes['review-box']}>
       <HeaderMedium>What people are saying</HeaderMedium>
       <Stars />
       <ScrollContainer noPadding = {true}>
-        <Feedback stars = {5}/>
-        <Feedback stars = {2}/>
-        <Feedback stars = {4}/>
-        <Feedback stars = {3}/>
-        <Feedback stars = {4}/>
-        <Feedback stars = {4}/>
-        <Feedback stars = {5}/>
+        {displayFeedBack()}
       </ScrollContainer>
     </div>
   );
