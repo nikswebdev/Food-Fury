7a59b8eb09d46672525f10e1038b3dce1a189282
nikswebdev <nikswebdevacc@gmail.com>
2022-01-08 05:50:00 -0600

    styled the MenuSection container

--- src/Menu/MenuSection/MenuSection.js
+++ src/Menu/MenuSection/MenuSection.js
@@ -3,12 +3,21 @@ import { Fragment } from 'react';
 import MenuHeader from '../MenuHeader/MenuHeader';
 import MenuCard from '../MenuCard/MenuCard';
 
-const MenuSection = ()=>{
-    return(
-        <Fragment>
-            <MenuHeader/>
-            <MenuCard/>
-        </Fragment>
-    );
-}
+const MenuSection = () => {
+  return (
+    <Fragment>
+      <MenuHeader />
+      <div className= {classes.menu__section}>
+        <MenuCard />
+        <MenuCard />
+        <MenuCard />
+        <MenuCard />
+        <MenuCard />
+        <MenuCard />
+        <MenuCard />
+    
+      </div>
+    </Fragment>
+  );
+};
 export default MenuSection;
