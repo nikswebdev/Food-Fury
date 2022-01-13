f3ac71d1a47da87eebd55aa8ce6da05d8b69fefa
nikswebdev <nikswebdevacc@gmail.com>
2022-01-12 19:31:06 -0600

    finished styling checkout cart

--- src/Cart/CartCheckout.js
+++ src/Cart/CartCheckout.js
@@ -6,11 +6,36 @@ const CartCheckout = () => {
   return (
     <div className={classes.cart__checkout}>
       <div className={classes['cart__checkout--header']}>
-        <p>Your cart from</p>
+        <p className={classes['cart__checkout--text']}>Your cart from</p>
         <h2>Red Robin Gourmet Burger and Brews</h2>
       </div>
 
-      <BtnCounter>Checkout $21.09</BtnCounter>
+      <BtnCounter>
+        Checkout <span>$21.09</span>
+      </BtnCounter>
+
+      <h2 className={classes['cart__checkout--header--text']}>This is a delivery order</h2>
+
+      <p className={classes['cart__checkout--message']}>
+        {' '}
+        Sit back, relax and a Food Fury delivery driver will take care of the annoying stuff for you!
+      </p>
+      <div className={classes['cart__checkout--item--container']}>
+        <div className={classes['cart__checkout--item--description']}>
+          <div className={classes['cart__checkout--item--amount']}>
+            <span>3</span>x
+          </div>
+          <div className={classes['cart__checkout--item--name-cost']}>
+            <div className={classes['cart__checkout--item--name']}> Pretzel Bites</div>
+            <div className={classes['cart__checkout--item--cost']}>
+              $<span>21.09</span>
+            </div>
+          </div>
+        </div>
+        
+        <div className={classes['cart__checkout--item--trash--container']}>trash</div>
+      </div>
+      
     </div>
   );
 };
